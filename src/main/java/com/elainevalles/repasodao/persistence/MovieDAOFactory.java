018f053129f143604065565221aeb958043aab1f
elainehello <elaineown@hotmail.es>
2025-04-25 20:27:15 +0200

    Delete src/main/java/com/elainevalles/repasodao directory

--- src/main/java/com/elainevalles/repasodao/persistence/MovieDAOFactory.java
+++
@@ -1,8 +0,0 @@
-package com.elainevalles.repasodao.persistence;
-
-public interface MovieDAOFactory {
-    public static IMovieDAO getMovieDAO() {
-        return new MovieDAOImplFake();
-
-    }
-}
